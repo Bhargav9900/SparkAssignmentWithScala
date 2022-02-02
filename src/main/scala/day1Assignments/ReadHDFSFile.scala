8cce6e66ab1bd93e27526aab47bdc178c2c7ae6c
Bhargav9900 <bhargavbechara@gmail.com>
2022-02-02 14:46:22 +0530

    updated

--- src/main/scala/day1Assignments/ReadHDFSFile.scala
+++ src/main/scala/day1Assignments/ReadHDFSFile.scala
@@ -13,10 +13,35 @@ object ReadHDFSFile {
       .config("spark.history.fs.logDirectory", "file:////home/sterlite/Spark/spark-events")
       .getOrCreate()
 
+    //read hdfs file
     val df = sparkSession.read.format(source = "csv").option("header", "true")
       .load(path = "hdfs://localhost:54310/dataset/employee/employee_address_details.csv")
 
+    //show 20 records from hdfs file
     df.show()
 
+    //store all display record in hdfs
+    df.write.csv("hdfs://localhost:54310/dataset/employee/StoredRecord")
+
+    //filter records
+    df.filter(df("Region") === "Northeast").show()
+    df.filter(df("Region") === "South").show()
+    df.filter(df("Region") === "Midwest").show()
+    df.filter(df("Region") === "West").show()
+    df.filter(df("Region") === "East").show()
+
+    //store data region wise
+    val df_ne = df.filter(df("Region") === "Northeast")
+    df_ne.write.csv("hdfs://localhost:54310/dataset/employee/empdata/Region=Northeast")
+
+    val df_s = df.filter(df("Region") === "South")
+    df_s.write.csv("hdfs://localhost:54310/dataset/employee/empdata/Region=South")
+
+    val df_mw = df.filter(df("Region") === "Midwest")
+    df_mw.write.csv("hdfs://localhost:54310/dataset/employee/empdata/Region=Midwest")
+
+
+    //using partition
+    df.write.partitionBy("Region").csv("hdfs://localhost:54310/dataset/employee/empdataPartition")
   }
 }
